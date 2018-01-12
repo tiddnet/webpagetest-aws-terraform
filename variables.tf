31ade466140d8ac5859246291d363ab51f44138b
Todd C. Campbell <tiddnet@users.noreply.github.com>
2018-01-12 01:37:14 -0500

    Update variables.tf
    replaced aws cred variables

--- variables.tf
+++ variables.tf
@@ -1,5 +1,9 @@
-variable "access_key_id" {}
-variable "secret_access_key" {}
+# Configure the AWS Provider
+provider "aws" {
+  shared_credentials_file = "~/.aws/credentials"
+  region                  = "${var.region}"
+}
+
 variable "webpagetest_api_key" { default = "sad43432reHH434dsad" }
 variable "region" { default = "eu-west-1" }
 variable "user_data" {}
