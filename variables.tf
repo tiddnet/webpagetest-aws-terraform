e4fd926d2864152e0a5d83b1c79f3c50f4831e9c
Todd C. Campbell <tiddnet@users.noreply.github.com>
2018-01-12 01:38:53 -0500

    removed

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-# Configure the AWS Provider
-provider "aws" {
-  shared_credentials_file = "~/.aws/credentials"
-  region                  = "${var.region}"
-}
-
 variable "webpagetest_api_key" { default = "sad43432reHH434dsad" }
 variable "region" { default = "eu-west-1" }
 variable "user_data" {}
